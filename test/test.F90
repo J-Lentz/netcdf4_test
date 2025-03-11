d466de87d37d219e0ba4f73364f4f5b544f58d55
Jesse Lentz <jesse.lentz@noaa.gov>
2025-03-11 13:16:14 -0400

    Initial commit

---
+++ test/test.F90
@@ -0,0 +1,135 @@
+#define KIND_ r8_kind
+
+program test_netcdf_perf
+
+use fms_mod, only: fms_init, fms_end, check_nml_error
+use fms2_io_mod, only: FmsNetcdfDomainFile_t, open_file, close_file, flush_file, &
+                       register_axis, register_variable_attribute, write_data, &
+                       read_data, unlimited
+use netcdf_io_mod, only: netcdf_add_variable
+use mpp_mod, only: mpp_error, fatal, stderr, input_nml_file
+use mpp_domains_mod, only : mpp_domains_set_stack_size, mpp_define_domains, &
+                            mpp_define_io_domain, mpp_get_compute_domain, &
+                            mpp_get_data_domain, domain2d
+use mpp_memutils_mod, only: mpp_print_memuse_stats
+use fms_string_utils_mod, only: string
+use random_numbers_mod, only: randomNumberStream, initializeRandomNumberStream, &
+                              getRandomNumbers
+use platform_mod, only: r4_kind, r8_kind
+
+implicit none
+
+integer :: nx
+integer :: ny
+integer :: niter
+character(*), parameter :: filename="data.nc"
+integer, dimension(2) :: layout, io_layout
+integer, dimension(3) :: chunksizes
+logical :: use_collective
+
+type(domain2d) :: domain
+integer :: is, ie, js, je !< Data domain
+integer :: isc, iec, jsc, jec !< Compute domain
+
+type(FmsNetcdfDomainFile_t) :: fileobj
+
+namelist /test_nml/ nx, ny, niter, layout, io_layout, chunksizes, use_collective
+
+call fms_init
+
+call read_test_nml
+fileobj%use_collective = use_collective
+
+call define_domain
+
+#ifdef WRITE_TEST
+call write_netcdf_file
+!call report_memuse("write")
+#endif
+
+#ifdef READ_TEST
+call read_netcdf_file
+!call report_memuse("read")
+#endif
+
+call fms_end
+
+contains
+
+subroutine define_domain
+  integer, parameter :: stack_size = 17280000
+
+  call mpp_domains_set_stack_size(stack_size)
+  call mpp_define_domains( [1, nx, 1, ny], layout, domain)
+  call mpp_define_io_domain(domain, io_layout)
+
+  call mpp_get_data_domain(domain, is, ie, js, je)
+  call mpp_get_compute_domain(domain, isc, iec, jsc, jec)
+end subroutine define_domain
+
+subroutine read_test_nml
+  integer :: ierr
+
+  read (input_nml_file, nml=test_nml, iostat=ierr)
+  ierr = check_nml_error(ierr, 'test_nml')
+end subroutine read_test_nml
+
+subroutine test_netcdf_file_open(mode)
+  character(*), intent(in) :: mode
+
+  if (.not.open_file(fileobj, filename, mode, domain)) then
+    call mpp_error(FATAL, "Failed to open netcdf file: " // filename)
+  endif
+
+  call register_axis(fileobj, "x", "x")
+  call register_axis(fileobj, "y", "y")
+  call register_axis(fileobj, "iter", unlimited)
+end subroutine test_netcdf_file_open
+
+subroutine write_netcdf_file
+  integer, parameter :: seed = 0
+  type(randomNumberStream) :: random_stream !> Random number stream
+  integer :: iter
+
+  real(KIND_), dimension(isc:iec, jsc:jec) :: rand
+
+  random_stream = initializeRandomNumberStream(seed)
+
+  call test_netcdf_file_open("write")
+
+  call netcdf_add_variable(fileobj, "random", "double", dimensions=["x", "y", "iter"], &
+                           chunksizes=chunksizes)
+
+  do iter=1,niter
+    call getRandomNumbers(random_stream, rand)
+    call write_data(fileobj, "random", rand, unlim_dim_level=iter)
+  enddo
+
+  call flush_file(fileobj)
+  call close_file(fileobj)
+end subroutine write_netcdf_file
+
+subroutine read_netcdf_file
+  integer :: iter
+  real(KIND_), dimension(is:ie, js:je) :: rand
+
+  call test_netcdf_file_open("read")
+
+  do iter=1,niter
+    call read_data(fileobj, "random", rand, unlim_dim_level=iter)
+  enddo
+
+  call close_file(fileobj)
+end subroutine read_netcdf_file
+
+!subroutine report_memuse(suffix)
+  !character(*), intent(in) :: suffix
+  !integer :: unit
+
+  !open(newunit=unit, file="memuse." // suffix, action="write", status="new")
+  !call mpp_memuse_end("NetCDF-4 test", unit)
+  !close(unit)
+  !call mpp_print_memuse_stats("netcdf test")
+!end subroutine report_memuse
+
+end program test_netcdf_perf
---
+++ test/test.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+RUNDIR=$SCRATCH/$USER/gfdl_f/test_netcdf4
+
+LOG=`pwd`/perf.log
+WRITE_PROG=`pwd`/test_write
+READ_PROG=`pwd`/test_read
+
+SRUN=`which srun`
+
+rm -rf $RUNDIR
+mkdir $RUNDIR
+
+echo "test_id,nx,ny,layout_x,layout_y,layout_io_x,layout_io_y,niter,netcdf_format,chunk_size_x,chunk_size_y,chunk_size_z,deflate level,shuffle,use_collective,file_size,write_time,read_time" >${LOG}
+
+i=0
+
+run_test () {
+  nx=$1
+  ny=$1
+  layout_x=$2
+  layout_y=$2
+  layout_io_x=$3
+  layout_io_y=$3
+  niter=$4
+  netcdf_format=$5
+  chunksize_x=$6
+  chunksize_y=$6
+  chunksize_z=1
+  deflate_level=$7
+  shuffle=$8
+  use_collective=$9
+
+  npes=$((layout_x * layout_y))
+  npes_io=$((layout_io_x * layout_io_y))
+
+  # Skip impossible layouts
+  [[ $((nx % layout_x)) != 0 ]] && return 0
+  [[ $((ny % layout_y)) != 0 ]] && return 0
+
+  # Skip impossible I/O layouts
+  [[ $((nx % layout_io_x)) != 0 ]] && return 0
+  [[ $((ny % layout_io_y)) != 0 ]] && return 0
+
+  [[ $((layout_x % layout_io_x)) != 0 ]] && return 0
+  [[ $((layout_y % layout_io_y)) != 0 ]] && return 0
+
+  # Skip impossible chunk sizes
+  [[ $(( (nx / layout_io_x) % chunksize_x)) != 0 ]] && return 0
+  [[ $(( (ny / layout_io_y) % chunksize_y)) != 0 ]] && return 0
+  
+  # Skip the collective I/O test if the I/O layout is 1x1
+  [[ "$use_collective" = "true" && $npes_io == 1 ]] && return 0
+
+  (( i++ ))
+
+  echo "Running test $i"
+  mkdir ${RUNDIR}/${i}
+  cd ${RUNDIR}/${i}
+
+  cat >input.nml <<EOF
+&test_nml
+    nx = $nx
+    ny = $ny
+    niter = $niter
+    layout = $layout_x , $layout_y
+    io_layout = $layout_io_x , $layout_io_y
+    chunksizes = ${chunksize_x}, ${chunksize_y}, ${chunksize_z}
+    use_collective = .${use_collective}.
+/
+
+&fms2_io_nml
+    netcdf_default_format = "$netcdf_format"
+    deflate_level         = $deflate_level
+    shuffle               = .${shuffle}.
+/
+EOF
+
+  $SRUN --ntasks=${npes} $WRITE_PROG |& tee write.log
+
+  if [ ${PIPESTATUS[0]} = 0 ]
+  then
+    echo "Finished writing data"
+  else
+    echo "write failure" | tee err.write
+    return 1
+  fi
+
+  $SRUN --ntasks=${npes} $READ_PROG |& tee read.log
+
+  if [ ${PIPESTATUS[0]} = 0 ]
+  then
+    echo "Finished reading data"
+  else
+    echo "read failure" | tee err.read
+    return 1
+  fi
+
+  write_time=`awk '/Total runtime/ {print $6}' write.log`
+  read_time=`awk '/Total runtime/ {print $6}' read.log`
+  file_size=`ls -lh data.nc | awk '{print $5}'`
+
+  echo "${i},${nx},${ny},${layout_x},${layout_y},${layout_io_x},${layout_io_y},${niter},${netcdf_format},${chunksize_x},${chunksize_y},${chunksize_z},${deflate_level},${shuffle},${use_collective},${file_size},${write_time},${read_time}" >>${LOG}
+}
+
+for n in 96 384 # 3072
+do
+  for layout in 1 2 3 6 12 24 32
+  do
+    for layout_io in 1 2 3 6 12 24
+    do
+      run_test $n $layout $layout_io 1 64bit 1 0 false false
+
+      for chunksize in 1 2 3 4
+      do
+        for deflate_level in `seq 0 9`
+        do
+          for shuffle in true false
+          do
+            for use_collective in true false
+            do
+              run_test $n $layout $layout_io 1 netcdf4 $chunksize $deflate_level $shuffle $use_collective
+            done
+	  done
+        done
+      done
+    done
+  done
+done
